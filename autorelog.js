cdebd5563860cd64c2b3b08ac86703a79b97c6cc
lillyBC <78206740+lillyBC@users.noreply.github.com>
2021-04-09 14:52:16 -0700

    Create autorelog.js

---
+++ autorelog.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Auto relog
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @author       Lilly
+// @match        https://www.bondageprojects.elementfx.com/*/BondageClub/
+// @match        *://www.bondageprojects.com/college/*/BondageClub/
+// @match        https://www.bondageprojects.com/college/*/*/
+// @match        https://www.bondageprojects.elementfx.com/*/*/
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    //changing various parts of the game's code by replacing entire functions with altered ones
+    function main() {
+        autoRelog = function(password){
+            ServerSocket.on("connect", function (){
+                console.log("getting back in");
+                setTimeout(function(){
+                    ElementValue("InputPassword",password)
+                    RelogSend()
+                },2000)
+
+            })
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+        console.log("auto login done")
+    }
+
+    //injecting the code in a <script> element into the website DOM
+    var script = document.createElement("script");
+    script.appendChild(document.createTextNode('('+ main +')();'));
+    document.body.appendChild(script);
+
+})();
